71b86e74facdbe1ff98bb20f8afff3a3644e092d
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-11-08 08:45:07 +0800

    测试小程序是否可用

--- client/src/pages/index/index.js
+++ client/src/pages/index/index.js
@@ -11,8 +11,7 @@ export default class Index extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      status: false,
-      pageIndex: null,
+      
     }
   }
 
@@ -27,46 +26,10 @@ export default class Index extends Component {
 
   componentWillUnmount() { }
 
-  isLogin = () => {
-    return {
-      openid: '123456',
-      nickname: 'zhangmin',
-      headimgurl: '',
-      guess: {
-        id: 1,
-        mobile: '15558018857',
-        amount: '1024.65'
-      }
-    }
-  }
-
-  guess = () => {
-    
-  }
-
-  toggle = () => {
-    this.setState({
-      pageIndex: 1
-    })
-  }
-
   render() {
-    let page = null
-    if (this.state.status && this.state.pageIndex === 0) {
-      page = <View className='page'>
-        pageOne
-              </View>
-
-    } else if (this.state.status && this.state.pageIndex === 1) {
-      page = <View className='page'>
-        pageOne
-    </View>
-    } else {
-      page = '11'
-    }
     return (
       <View className='container'>
-        {page}
+        1111
       </View>
     )
   }
